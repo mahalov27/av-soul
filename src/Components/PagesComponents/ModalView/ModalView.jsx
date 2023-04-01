dd765477550e0e9e8c2963ef9955bb13221eb34e
Makhalov Maksim <makhalovmaksym@gmail.com>
2023-04-02 00:06:19 +0300

    added swipe effect in ModalView

--- src/Components/PagesComponents/ModalView/ModalView.jsx
+++ src/Components/PagesComponents/ModalView/ModalView.jsx
@@ -1,56 +1,100 @@
 import { useState, useEffect } from "react";
-import { nextIndex, prevIndex} from "../../services/indexCounter.js"
+import { motion, AnimatePresence } from "framer-motion";
+import { getNewIndex } from "../../services/indexCounter";
 import styles from "./ModalView.module.css";
+
+const variants = {
+  enter: (direction) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+};
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset, velocity) => {
+  return Math.abs(offset) * velocity;
+};
 
 const ModalView = (props) => {
   const { list, func, idx } = props;
-  const [indexImages, setIndexImages] = useState(0);
-  const [startX, setStartX] = useState(0)
+  const [[currentIndex, step], setPage] = useState([Number(idx), 0]);
 
   useEffect(() => {
-    setIndexImages(Number(idx));
-  }, [idx]);
+    setPage([Number(idx), Number(idx)])
+  },[idx])
 
-  const toNextIndex = () => {
-    setIndexImages(nextIndex(list.length, indexImages))
+  const paginate = (newStep) => {
+    setPage([getNewIndex(list.length, currentIndex, newStep), newStep]);
   };
 
-  const toPrevIndex = () => {
-    setIndexImages(prevIndex(list.length, indexImages))
-  };
-
-  const testOnDrag = (e) => {
-     const endX = e.clientX;
-     if (endX > startX) {
-      toPrevIndex()
-    } else if (endX < startX) {
-      toNextIndex()
-    }
-  }
-
-  const testStartX = (e) => {
-    setStartX(e.clientX)
-  }
 
   return (
     <div className={styles.modal}>
-      <button type="button" className={styles.btn} onClick={toPrevIndex}>
+      <button
+        type="button"
+        className={styles.btn + " " + styles.prevBtn}
+        onClick={() => {
+          paginate(-1);
+        }}
+      >
         <img
-          src={process.env.PUBLIC_URL + '/images/icons/arrow.png'}
+          src={process.env.PUBLIC_URL + "/images/icons/arrow.png"}
           className={styles.img}
-          alt="button to previous"  
+          alt="button to previous"
         />
       </button>
-      <img
-        src={process.env.PUBLIC_URL + list[indexImages].src}
-        alt={list[indexImages].alt.ua}
-        className={styles.viewImg}
-        onDragEnd={testOnDrag}
-        onDragStart={testStartX}
-      />
-      <button type="button" className={styles.btn + " " + styles.nextBtn} onClick={toNextIndex}>
+      <AnimatePresence initial={false} custom={step}>
+        <motion.img
+          key={currentIndex}
+          src={process.env.PUBLIC_URL + list[currentIndex].src}
+          alt={list[currentIndex].alt.ua}
+          className={styles.viewImg}
+          custom={step}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
+
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1);
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1);
+            }
+          }}
+        />
+      </AnimatePresence>
+      <button
+        type="button"
+        className={styles.btn + " " + styles.nextBtn}
+        onClick={() => {
+          paginate(1);
+        }}
+      >
         <img
-          src={process.env.PUBLIC_URL + '/images/icons/arrow.png'}
+          src={process.env.PUBLIC_URL + "/images/icons/arrow.png"}
           className={styles.img}
           alt="button to next"
         />
@@ -60,7 +104,11 @@ const ModalView = (props) => {
         className={styles.closeBtn + " " + styles.btn}
         onClick={func}
       >
-        <img src={process.env.PUBLIC_URL + '/images/icons/close.png'} alt="close button" className={styles.img} />
+        <img
+          src={process.env.PUBLIC_URL + "/images/icons/close.png"}
+          alt="close button"
+          className={styles.img}
+        />
       </button>
     </div>
   );
--- src/Components/services/indexCounter.js
+++ src/Components/services/indexCounter.js
@@ -5,3 +5,19 @@ export const nextIndex = (arrayLenght, index) => {
 export const prevIndex = (arrayLenght, index) => {
   return index > 0 ? index - 1 : arrayLenght - 1;
 };
+
+export const getNewIndex = (arrayLenght, currentIndex, step) => {
+  if(step > 0){
+    if(arrayLenght - step > currentIndex){
+      return currentIndex + step;
+    }else{
+      return 0;
+    }
+  }else{
+    if(currentIndex > 0){
+      return currentIndex + step;
+    }else{
+      return arrayLenght - 1;
+    }
+  }
+}
