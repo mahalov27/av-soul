a59a55173e68a3e9a6650617f947c059c5058d30
Makhalov Maksim <makhalovmaksym@gmail.com>
2023-05-02 14:01:02 +0300

    Updates

---
+++ static/js/19.a6c83584.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkav_soul=self.webpackChunkav_soul||[]).push([[19],{5019:function(u,n,e){e.r(n),e.d(n,{default:function(){return f}});var c=e(2791),s={},t=e(184),f=function(){return(0,c.useEffect)((function(){}),[]),(0,t.jsx)("h1",{className:s.h,children:"User Account"})}}}]);
+//# sourceMappingURL=19.a6c83584.chunk.js.map
---
+++ static/js/467.1ce87669.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkav_soul=self.webpackChunkav_soul||[]).push([[467],{8467:function(e,n,u){u.r(n);var a=u(184);n.default=function(){return(0,a.jsx)(a.Fragment,{children:"AdminPage"})}}}]);
+//# sourceMappingURL=467.1ce87669.chunk.js.map
